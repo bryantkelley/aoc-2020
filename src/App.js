1fb9fc161ccc5133c0e79acc293ff247811012d9
Bryant Kelley <bryant@bryantkelley.com>
2020-12-05 12:33:38 -0600

    Setup react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSleigh } from '@fortawesome/free-solid-svg-icons';
+import Home from './home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar>
+        <Navbar.Brand href="/">
+          <FontAwesomeIcon icon={faSleigh} />
+          {' '}
+          AOC-2020
+        </Navbar.Brand>
+        <Nav defaultActiveKey="/">
+          <Nav.Link href="/">Home</Nav.Link>
+          {/* <NavDropdown title="Days">
+            <NavDropdown.Item href="/1">One</NavDropdown.Item>
+          </NavDropdown> */}
+        </Nav>
+      </Navbar>
+      <Container>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 }
 
---
+++ src/home.js
@@ -0,0 +1,13 @@
+import { Col, Row } from "react-bootstrap";
+
+function Home() {
+  return (
+    <Row>
+      <Col>
+        <div>Bryant's Advent of Code 2020</div>
+      </Col>
+    </Row>
+  );
+}
+
+export default Home;
