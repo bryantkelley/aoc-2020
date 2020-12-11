1f5605671f97410521071ad176abbfde5af0a773
Bryant Kelley <bryant@bryantkelley.com>
2020-12-10 23:57:53 -0600

    Day 11 Part 1

--- src/11/eleven.js
+++ src/11/eleven.js
@@ -9,7 +9,7 @@ function Eleven() {
 
   useEffect(() => {
     async function getEntries() {
-      fetch(raw).then(r => r.text()).then(text => text.split('\n')).then((arr) => {
+      fetch(raw).then(r => r.text()).then(text => text.split('\n').map(line => line.split(''))).then((arr) => {
         setEntries(arr);
       });
     }
@@ -17,6 +17,93 @@ function Eleven() {
     if (!entries.length) {
       getEntries();
     }
+  }, [entries]);
+
+  const resultOne = useMemo(() => {
+    if (!entries.length) {
+      return '';
+    }
+
+    let floorplan = [entries];
+    let changing = true;
+
+    while (changing) {
+      let noChanges = true;
+      const current = floorplan.length - 1;
+      function checkNeighbors(ri, ci) {
+        let occupiedCount = 0;
+        // Top Left
+        if (ri > 0 && ci > 0 && floorplan[current][ri - 1][ci - 1] === '#') {
+          occupiedCount = occupiedCount + 1;
+        }
+        // Top Middle
+        if (ri > 0 && floorplan[current][ri - 1][ci] === '#') {
+          occupiedCount = occupiedCount + 1;
+        }
+        // Top Right
+        if (ri > 0 && ci < floorplan[current][ri].length - 1 && floorplan[current][ri - 1][ci + 1] === '#') {
+          occupiedCount = occupiedCount + 1;
+        }
+        // Middle Left
+        if (ci > 0 && floorplan[current][ri][ci - 1] === '#') {
+          occupiedCount = occupiedCount + 1;
+        }
+        // Middle Right
+        if (ci < floorplan[current][ri].length - 1 && floorplan[current][ri][ci + 1] === '#') {
+          occupiedCount = occupiedCount + 1;
+        }
+        // Bottom Left
+        if (ri < floorplan[current].length - 1 && ci > 0 && floorplan[current][ri + 1][ci - 1] === '#') {
+          occupiedCount = occupiedCount + 1;
+        }
+        // Bottom Middle
+        if (ri < floorplan[current].length - 1 && floorplan[current][ri + 1][ci] === '#') {
+          occupiedCount = occupiedCount + 1;
+        }
+        // Bottom Right
+        if (ri < floorplan[current].length - 1 && ci < floorplan[current][ri].length - 1 && floorplan[current][ri + 1][ci + 1] === '#') {
+          occupiedCount = occupiedCount + 1;
+        }
+
+        if (floorplan[current][ri][ci] === 'L' && occupiedCount === 0) {
+          noChanges = false;
+          return '#';
+        }
+        if (floorplan[current][ri][ci] === '#' && occupiedCount >= 4) {
+          noChanges = false;
+          return 'L';
+        }
+        return floorplan[current][ri][ci];
+      }
+
+      floorplan.push(floorplan[current].map((row, ri) => row.map((cell, ci) => {
+        if (floorplan[current][ri][ci] === '.') {
+          return '.';
+        }
+        return checkNeighbors(ri, ci);
+      })));
+
+      if (noChanges) {
+        changing = false;
+      }
+    }
+
+    let count = 0;
+
+    floorplan[floorplan.length - 1].forEach(row => row.forEach((cell) => {
+      if (cell === '#') {
+        count = count + 1;
+      }
+    }));
+
+    return count;
+  }, [entries]);
+
+  const resultTwo = useMemo(() => {
+    if (!entries.length) {
+      return '';
+    }
+    return 'Coming Soon'
   }, [entries]);
 
   return (
@@ -32,8 +119,8 @@ function Eleven() {
             <h3>Part 1</h3>
           </Col>
           <Col>
-            <h4> </h4>
-            <h5>{}</h5>
+            <h4>Estimated Available Seats</h4>
+            <h5>{resultOne}</h5>
           </Col>
           <Col>
             <h4>
@@ -46,8 +133,8 @@ function Eleven() {
             <h3>Part 2</h3>
           </Col>
           <Col>
-            <h4> </h4>
-            <h5>{}</h5>
+            <h4>Actual Available Seats</h4>
+            <h5>{resultTwo}</h5>
           </Col>
           <Col>
             <h4>
