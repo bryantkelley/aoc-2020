70fe7187b00358fbd8f22449a2abf9d9a38e3c3a
Bryant Kelley <bryant@bryantkelley.com>
2020-12-12 15:35:55 -0600

    Day 11 Part 2

--- src/11/eleven.js
+++ src/11/eleven.js
@@ -103,7 +103,111 @@ function Eleven() {
     if (!entries.length) {
       return '';
     }
-    return 'Coming Soon'
+    // 2449 is too high
+    
+    console.log(entries);
+
+    let floorplan = [entries];
+    console.log(floorplan);
+    let changing = true;
+
+    while (changing) {
+      let noChanges = true;
+      const current = floorplan.length - 1;
+
+      // ( current row, current column, row modifier, column modifier )
+      // returns 0 if 'L' or 1 if '#'
+      // if '.' and next row or column is out of bounds return 0
+      // else call checkNeighbor with next value
+      function checkNeighbor(cri, cci, rm, cm) {
+        const valueToCheck = floorplan[current][cri][cci];
+        if (valueToCheck === 'L') {
+          return 0;
+        }
+        if (valueToCheck === '#') {
+          return 1;
+        }
+
+        const nextRow = cri + rm;
+        const nextColumn = cci + cm;
+
+        if (
+          nextRow < 0 || floorplan[current].length <= nextRow ||
+          nextColumn < 0 || floorplan[current][nextRow].length <= nextColumn
+        ) {
+          return 0;
+        }
+        
+        return checkNeighbor(nextRow, nextColumn, rm, cm);
+      }
+
+      function checkNeighbors(ri, ci) {
+        let occupiedCount = 0;
+        // Top Left
+        if (ri > 0 && ci > 0) {
+          occupiedCount = occupiedCount + checkNeighbor(ri - 1, ci - 1, -1, -1);
+        }
+        // Top Middle
+        if (ri > 0) {
+          occupiedCount = occupiedCount + checkNeighbor(ri - 1, ci, -1, 0);
+        }
+        // Top Right
+        if (ri > 0 && ci < floorplan[current][ri].length - 1) {
+          occupiedCount = occupiedCount + checkNeighbor(ri - 1, ci + 1, -1, 1);
+        }
+        // Middle Left
+        if (ci > 0) {
+          occupiedCount = occupiedCount + checkNeighbor(ri, ci - 1, 0, -1);
+        }
+        // Middle Right
+        if (ci < floorplan[current][ri].length - 1) {
+          occupiedCount = occupiedCount + checkNeighbor(ri, ci + 1, 0, 1);
+        }
+        // Bottom Left
+        if (ri < floorplan[current].length - 1 && ci > 0) {
+          occupiedCount = occupiedCount + checkNeighbor(ri + 1, ci - 1, 1, -1);
+        }
+        // Bottom Middle
+        if (ri < floorplan[current].length - 1) {
+          occupiedCount = occupiedCount + checkNeighbor(ri + 1, ci, 1, 0);
+        }
+        // Bottom Right
+        if (ri < floorplan[current].length - 1 && ci < floorplan[current][ri].length - 1) {
+          occupiedCount = occupiedCount + checkNeighbor(ri + 1, ci + 1, 1, 1);
+        }
+
+        if (floorplan[current][ri][ci] === 'L' && occupiedCount === 0) {
+          noChanges = false;
+          return '#';
+        }
+        if (floorplan[current][ri][ci] === '#' && occupiedCount >= 5) {
+          noChanges = false;
+          return 'L';
+        }
+        return floorplan[current][ri][ci];
+      }
+
+      floorplan.push(floorplan[current].map((row, ri) => row.map((cell, ci) => {
+        if (floorplan[current][ri][ci] === '.') {
+          return '.';
+        }
+        return checkNeighbors(ri, ci);
+      })));
+
+      if (noChanges) {
+        changing = false;
+      }
+    }
+
+    let count = 0;
+
+    floorplan[floorplan.length - 1].forEach(row => row.forEach((cell) => {
+      if (cell === '#') {
+        count = count + 1;
+      }
+    }));
+
+    return count;
   }, [entries]);
 
   return (
