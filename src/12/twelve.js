2762aaa90112b998694becc8b9e9de35fdf5c172
Bryant Kelley <bryant@bryantkelley.com>
2020-12-12 16:13:58 -0600

    Day 12 Part 1

--- src/12/twelve.js
+++ src/12/twelve.js
@@ -9,7 +9,7 @@ function Twelve() {
 
   useEffect(() => {
     async function getEntries() {
-      fetch(raw).then(r => r.text()).then(text => text.split('\n')).then((arr) => {
+      fetch(raw).then(r => r.text()).then(text => text.split('\n').map(line => ({ action: line.substring(0, 1), value: parseInt(line.substring(1)) }))).then((arr) => {
         setEntries(arr);
       });
     }
@@ -23,7 +23,58 @@ function Twelve() {
     if (!entries.length) {
       return '';
     }
+    
+    // directions => ['east', 'south', 'west', 'north']
+    let direction = 0;
+    let x = 0; // east/west distance
+    let y = 0; // north/south distance
 
+    entries.forEach((line) => {
+      const { action, value } = line;
+      if (action === 'N') {
+        // Move +y
+        y = y + value;
+      }
+      if (action === 'S') {
+        // Move -y
+        y = y - value;
+      }
+      if (action === 'E') {
+        // Move +x
+        x = x + value;
+      }
+      if (action === 'W') {
+        // Move -x
+        x = x - value;
+      }
+      if (action === 'L') {
+        // Turn Left
+        const difference = direction - (parseInt(value /90) % 4);
+        direction = difference < 0 ? 4 + difference : difference;
+
+      }
+      if (action === 'R') {
+        // Turn Right
+        direction = (direction + parseInt(value / 90)) % 4;
+      }
+      if (action === 'F') {
+        // Move in direction
+        if (direction === 3) {
+          y = y + value;
+        }
+        if (direction === 1) {
+          y = y - value;
+        }
+        if (direction === 0) {
+          x = x + value;
+        }
+        if (direction === 2) {
+          x = x - value;
+        }
+      }
+    });
+
+    return Math.abs(x) + Math.abs(y);
   }, [entries]);
 
   const resultTwo = useMemo(() => {
@@ -46,7 +97,7 @@ function Twelve() {
             <h3>Part 1</h3>
           </Col>
           <Col>
-            <h4> </h4>
+            <h4>Distance from Start</h4>
             <h5>{resultOne}</h5>
           </Col>
           <Col>
