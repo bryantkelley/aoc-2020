5423ea281cc73ad2886dcfb04e92c31426cc0e60
Bryant Kelley <bryant@bryantkelley.com>
2020-12-13 14:23:09 -0600

    Add all the stub files so I can go back to using git add .

---
+++ src/25/twentyFive.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect, useMemo } from "react";
+import { Col, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGift, faGlassCheers } from '@fortawesome/free-solid-svg-icons';
+import raw from './input.txt';
+
+function TwentyFive() {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    async function getEntries() {
+      fetch(raw).then(r => r.text()).then(text => text.split('\n')).then((arr) => {
+        setEntries(arr);
+      });
+    }
+
+    if (!entries.length) {
+      getEntries();
+    }
+  }, [entries]);
+
+  const resultOne = useMemo(() => {
+    if (!entries.length) {
+      return '';
+    }
+
+  }, [entries]);
+
+  const resultTwo = useMemo(() => {
+    if (!entries.length) {
+      return '';
+    }
+    
+  }, [entries]);
+
+  return (
+    <Row>
+      <Col>
+        <Row>
+          <Col>
+            <h2>Day Twenty-Five</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>Part 1</h3>
+          </Col>
+          <Col>
+            <h4> </h4>
+            <h5>{resultOne}</h5>
+          </Col>
+          <Col>
+            <h4>
+              <FontAwesomeIcon icon={faGift} />
+            </h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>Part 2</h3>
+          </Col>
+          <Col>
+            <h4> </h4>
+            <h5>{resultTwo}</h5>
+          </Col>
+          <Col>
+            <h4>
+              <FontAwesomeIcon icon={faGlassCheers} />
+            </h4>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+}
+
+export default TwentyFive;
